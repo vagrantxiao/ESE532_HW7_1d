dac93cb1dfed62d9f2eb8e330098ae73782cd696
Yuanlong Xiao <ylxiao@quark.seas.upenn.edu>
2017-10-25 11:58:53 -0400

    first commit

---
+++ Differentiate.cpp
@@ -0,0 +1,43 @@
+#include "Pipeline.h"
+
+void Differentiate_SW(const unsigned char * Input, unsigned char * Output)
+{
+  for (int Y = 0; Y < OUTPUT_FRAME_HEIGHT; Y++)
+    for (int X = 0; X < OUTPUT_FRAME_WIDTH; X++)
+    {
+      int Average = 0;
+      if (Y > 0 && X > 0)
+        Average = (Input[OUTPUT_FRAME_WIDTH * (Y - 1) + X] + Input[OUTPUT_FRAME_WIDTH * Y + X - 1]) / 2;
+      else if (Y > 0)
+        Average = Input[OUTPUT_FRAME_WIDTH * (Y - 1) + X];
+      else if (X > 0)
+        Average = Input[OUTPUT_FRAME_WIDTH * Y + X - 1];
+
+      unsigned char Diff = Input[OUTPUT_FRAME_WIDTH * Y + X] - Average;
+
+      Output[Y * OUTPUT_FRAME_WIDTH + X] = Diff;
+    }
+}
+
+#pragma SDS data copy(Input[0:INPUT_FRAME_WIDTH*2], Output[0:INPUT_FRAME_WIDTH])
+#pragma SDS data access_pattern(Input:SEQUENTIAL, Output:SEQUENTIAL)
+void Differentiate_HW(const unsigned char Input[INPUT_FRAME_SIZE], unsigned char Output[INPUT_FRAME_SIZE])
+{
+  for (int Y = 0; Y < OUTPUT_FRAME_HEIGHT; Y++)
+    for (int X = 0; X < OUTPUT_FRAME_WIDTH; X++)
+    {
+      int Average = 0;
+      if (Y > 0 && X > 0)
+        Average = (Input[OUTPUT_FRAME_WIDTH * (Y - 1) + X] + Input[OUTPUT_FRAME_WIDTH * Y + X - 1]) / 2;
+      else if (Y > 0)
+        Average = Input[OUTPUT_FRAME_WIDTH * (Y - 1) + X];
+      else if (X > 0)
+        Average = Input[OUTPUT_FRAME_WIDTH * Y + X - 1];
+
+      unsigned char Diff = Input[OUTPUT_FRAME_WIDTH * Y + X] - Average;
+
+      Output[Y * OUTPUT_FRAME_WIDTH + X] = Diff;
+    }
+}
+
+
